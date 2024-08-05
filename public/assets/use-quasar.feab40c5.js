dfbf748fa48282acc67c4f5274ba5a6c4be1559c
unknown <blas_d619@hotmail.com>
2024-08-05 16:47:39 -0700

    Actualización de filtrados CapturePage y LandingPage

--- public/assets/use-quasar.feab40c5.js
+++
@@ -1,1 +0,0 @@
-import{i as a,S as r}from"./index.a1a9ce16.js";function u(){return a(r)}export{u};
--- routes/encuesta.route.js
+++ routes/encuesta.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { saveDataQuestionaire, getInfoCatCuestionario } from "../controllers/cuestionario.controller.js";
+import { saveDataQuestionaire, getInfoCatCuestionario, getInfoCatParroquia } from "../controllers/cuestionario.controller.js";
 import { requireToken } from "../middlewares/validationToken.js";
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 router.post("/forma/guardado", requireToken, saveDataQuestionaire);
 
 router.get("/forma/info", requireToken, getInfoCatCuestionario);
+
+router.get("/forma/infoParroquia", requireToken, getInfoCatParroquia);
 
 export default router;
